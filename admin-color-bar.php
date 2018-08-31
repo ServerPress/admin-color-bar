66f4454e9414a3b1bebd1c01efc8473411816cb7
Dave Jesch <dave@davejesch.com>
2018-08-30 18:58:07 -0700

    add css to front page requests #3

--- admin-color-bar.php
+++ admin-color-bar.php
@@ -3,9 +3,9 @@
  * Plugin Name: Admin Color Bar
  * Plugin URL: https://serverpress.com/plugins/admin-bar-color
  * Description: Changes the Admin bar color
- * Version: 1.2.1
- * Author: Gregg Franklin
- * Author URI: http://greggfranklin.com
+ * Version: 1.2.2
+ * Author: Gregg Franklin, Dave Jesch
+ * Author URI: http://ServerPress.com
  * Text Domain: admin-color-bar
  * Domain path: /language
  */
@@ -18,11 +18,12 @@ class DS_AdminColorBar
 	const SETTINGS_KEY = 'admin-color-bar-options';
 
 	const PLUGIN_NAME = 'Admin Color Bar';
-	const PLUGIN_VERSION = '1.2';
+	const PLUGIN_VERSION = '1.2.2';
 
 	private function __construct()
 	{
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'wp_head', array( $this, 'footer' ) );
 	}
 
 	public static function get_instance()
@@ -44,6 +45,33 @@ class DS_AdminColorBar
 		if ( is_admin_bar_showing() ) {
 			require_once( dirname( __FILE__ ) . '/admin-color-bar-admin.php' );
 		}
+	}
+	public function footer()
+	{
+		$settings = self::get_settings();
+		$color = empty( $settings['color'] ) ? '#0073AA' : $settings['color'];
+		$textcolor = empty( $settings['textcolor'] ) ? '#eee' : $settings['textcolor'];
+?>
+<style type='text/css'>
+	#wp-admin-bar-environment-notice>div,
+	#wpadminbar { background-color: <?php echo $color; ?> !important }
+	#wp-admin-bar-environment-notice { display: none }
+	@media only screen and (min-width: 1030px) {
+	    #wp-admin-bar-environment-notice { display: block }
+	    #wp-admin-bar-environment-notice > div > span {
+	        /*color: #EEE !important;*/
+	        font-size: 130% !important;
+	    }
+		#wpadminbar #adminbarsearch:before, #wpadminbar .ab-icon:before, #wpadminbar .ab-item:before {
+			color: <?php echo $textcolor; ?> !important;
+		}
+	}
+	#wpadminbar .ab-empty-item,#wpadminbar a.ab-item,#wpadminbar>#wp-toolbar span.ab-label,#wpadminbar>#wp-toolbar span.noticon{color:<?php echo $textcolor; ?>;}
+	#adminbarsearch:before,
+	.ab-icon:before,
+	.ab-item:before { color: #EEE !important }
+</style>
+<?php
 	}
 }
 
