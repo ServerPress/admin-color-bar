d5de4099b763281bd708a5a0cec665fa9e220b40
Gregg Franklin <greggfranklin@gmail.com>
2019-11-20 10:38:16 -0800

    Increassed version number

--- admin-color-bar.php
+++ admin-color-bar.php
@@ -3,7 +3,7 @@
  * Plugin Name: Admin Color Bar
  * Plugin URL: https://serverpress.com/plugins/admin-bar-color
  * Description: Changes the Admin bar color
- * Version: 1.2.2
+ * Version: 1.2.3
  * Author: Gregg Franklin, Dave Jesch
  * Author URI: http://ServerPress.com
  * Text Domain: admin-color-bar
